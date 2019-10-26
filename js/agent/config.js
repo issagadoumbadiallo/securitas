49e2ddfcfb5f06e75d1a5f33aee3cf15dc5d72d1
unknown <issagadoumbadiallo@gmail.com>
2019-10-26 19:04:10 +0200

    test

---
+++ js/agent/config.js
@@ -0,0 +1,10 @@
+window._config = {
+    cognito: {
+        userPoolId: 'eu-west-1_Uot434gOo', 
+        userPoolClientId: '7cn4qgvdjkabjdrmigkct4759j', 
+        region: 'eu-west-1' 
+    },
+    api: {
+        invokeUrl: 'eu-west-1:812340317506:userpool/eu-west-1_Uot434gOo' 
+    }
+};
---
+++ js/entreprise/config.js
@@ -0,0 +1,10 @@
+window._config = {
+    cognito: {
+        userPoolId: 'eu-west-1_32YYqyoDa', 
+        userPoolClientId: '1si9stk1jabvnsjh6lr7emm58h', 
+        region: 'eu-west-1' 
+    },
+    api: {
+        invokeUrl: 'https://bag49rrypi.execute-api.eu-west-1.amazonaws.com/prod'//'eu-west-1:812340317506:userpool/eu-west-1_32YYqyoDa'
+    }
+};
